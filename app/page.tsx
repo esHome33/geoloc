680495fa84417baa2b9fc16de0a13f5c89c7bff6
esHome33 <etienschwa@gmail.com>
2023-07-12 14:29:00 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+	const [lat, setLat] = useState<number | "non déterminé">("non déterminé");
+	const [lon, setLon] = useState<number | "non déterminé">("non déterminé");
+	const [alt, setAlt] = useState<string>("non déterminé");
+	const [accuracy, setAccuracy] = useState<string>("non déterminé");
+	const [speed, setSpeed] = useState<string>("non déterminé");
+	const [erreur, setErreur] = useState<string>("");
+
+	const [geoServices, setGeoServices] = useState(
+		"Services de géolocalisation non disponibles"
+	);
+
+	const getGeolocService = () => {
+		if ("geolocation" in navigator) {
+			setGeoServices("Services de géolocalisation disponibles");
+			return true;
+		} else {
+			setGeoServices("Services de géolocalisation non disponibles");
+			return false;
+		}
+	};
+
+	useEffect(() => {
+		if (getGeolocService()) {
+			const geoloc = navigator.geolocation;
+
+			const onSuccess = (pos: GeolocationPosition) => {
+				setLat(pos.coords.latitude);
+				setLon(pos.coords.longitude);
+				const acc = pos.coords.accuracy.toFixed() + " m";
+				setAccuracy(acc);
+				const vitesse = pos.coords.speed;
+				if (vitesse) {
+					setSpeed(vitesse + " m/s");
+				} else {
+					setSpeed("non déterminé");
+				}
+				const altit = pos.coords.altitude;
+				if (altit) {
+					setAlt(altit + " m");
+				} else {
+					setAlt("non déterminé");
+				}
+				setErreur("");
+			};
+
+			const onErr = () => {
+				setErreur("une erreur de géolocalisation est survenue");
+			};
+
+			geoloc.getCurrentPosition(onSuccess, onErr, {
+				maximumAge: 50,
+			});
+		}
+	}, []);
+
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<div className="flex flex-col space-y-10 mb-20">
+				<Typography
+					variant="h4"
+					className="text-amber-600"
+				>
+					Géolocation
+				</Typography>
+				<Typography
+					variant="body2"
+					className="italic"
+				>
+					{geoServices}
+				</Typography>
+				<Typography>Latitude = {lat}</Typography>
+				<Typography>Longitude = {lon}</Typography>
+				<Typography className="text-blue-600">Altitude = {alt}</Typography>
+				<Typography>précision = {accuracy}</Typography>
+				<Typography className="text-green-500">
+					Vitesse = {speed} m/s
+				</Typography>
+				<Typography className="mt-4">{erreur}</Typography>
+			</div>
+			<div className="text-center">
+				<Typography variant="body2">ESHome 33 - juillet 2023</Typography>
+			</div>
+		</main>
+	);
+}
